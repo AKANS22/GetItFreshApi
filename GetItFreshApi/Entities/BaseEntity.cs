17c33ef37f3c3508f3228a6a7e43e64cf45b1776
Akans Udoka <Akansudoka7@gmail.com>
2023-10-06 12:29:58 +0100

    Add project files.

---
+++ GetItFreshApi/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetItFreshApi.Entities
+{
+    public class BaseClass
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
