88c6365701282b6e5c0e30f19e82eee5e084986c
Akans Udoka <Akansudoka7@gmail.com>
2023-10-07 08:25:40 +0100

    Completed log

--- GetItFreshApi/Program.cs
+++ GetItFreshApi/Program.cs
@@ -1,26 +1,37 @@
 using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add services to the container.
 //Log.Logger = new LoggerConfiguration()
-//    .WriteTo.File(path: "C:\\winks\\GetItFreshApi\\GetItFreshApi\\Log\\Log.txt")
-//    .CreateLogger();
+//    .WriteTo.File(path: "C:\\winks\\GetItFreshApi\\GetItFreshApi\\Log\\Log.txt",
+//    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+//    rollingInterval: RollingInterval.Day,
+//    rollOnFileSizeLimit:true,
+//    restrictedToMinimumLevel: LogEventLevel.Information
+//    )
+//   .CreateLogger();
 
 var log = new LoggerConfiguration()
     .WriteTo.File("Logs/Log.txt",
     rollingInterval: RollingInterval.Day,
-    rollOnFileSizeLimit: true)
+    rollOnFileSizeLimit: true,
+    restrictedToMinimumLevel:LogEventLevel.Information)
     .CreateLogger();
 
 try
 {
+    log.Information("Application is starting");
+
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Host.UseSerilog();
 
     var app = builder.Build();
+    
 
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
@@ -28,6 +39,8 @@ try
         app.UseSwagger();
         app.UseSwaggerUI();
     }
+
+    app.UseSerilogRequestLogging();
 
     app.UseHttpsRedirection();
 
