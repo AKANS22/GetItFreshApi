1127af1c897dce5dd98cdd88ec43db213bfdc046
Akans Udoka <Akansudoka7@gmail.com>
2023-10-07 08:47:37 +0100

    Added Cors

--- GetItFreshApi/Entities/Transaction.cs
+++ GetItFreshApi/Entities/Transaction.cs
@@ -1,6 +1,13 @@
-﻿namespace GetItFreshApi.Entities
+﻿using GetItFreshApi.Entities.Enums;
+
+namespace GetItFreshApi.Entities
 {
-    public class Transaction
+    public class Transaction : BaseClass
     {
+        public string MerchantId { get; set; }
+        public Merchant Merchant { get; set; }
+        public decimal AmountPaid { get; set; }
+        public PurchaseStatus PurchaseStatus { get; set; }
+        public DateTime DateCompleted { get; set; } 
     }
 }
--- GetItFreshApi/Program.cs
+++ GetItFreshApi/Program.cs
@@ -4,25 +4,25 @@ using Serilog.Events;
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
-//Log.Logger = new LoggerConfiguration()
-//    .WriteTo.File(path: "C:\\winks\\GetItFreshApi\\GetItFreshApi\\Log\\Log.txt",
-//    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
-//    rollingInterval: RollingInterval.Day,
-//    rollOnFileSizeLimit:true,
-//    restrictedToMinimumLevel: LogEventLevel.Information
-//    )
-//   .CreateLogger();
-
-var log = new LoggerConfiguration()
-    .WriteTo.File("Logs/Log.txt",
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(path: "C:\\winks\\GetItFreshApi\\GetItFreshApi\\Log\\Log.txt",
+    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
     rollingInterval: RollingInterval.Day,
     rollOnFileSizeLimit: true,
-    restrictedToMinimumLevel:LogEventLevel.Information)
-    .CreateLogger();
+    restrictedToMinimumLevel: LogEventLevel.Information
+    )
+   .CreateLogger();
+
+//var log = new LoggerConfiguration()
+//    .WriteTo.File("Logs/Log.txt",
+//    rollingInterval: RollingInterval.Day,
+//    rollOnFileSizeLimit: true,
+//    restrictedToMinimumLevel:LogEventLevel.Information)
+//    .CreateLogger();
 
 try
 {
-    log.Information("Application is starting");
+    Log.Information("Application is starting");
 
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
