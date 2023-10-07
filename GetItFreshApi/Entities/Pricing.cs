a21bc5a93b1d9ff46b966289805c8770770c81d8
Akans Udoka <Akansudoka7@gmail.com>
2023-10-07 07:19:23 +0100

    Added serilog

--- GetItFreshApi/Entities/Pricing.cs
+++ GetItFreshApi/Entities/Pricing.cs
@@ -1,9 +1,11 @@
-﻿namespace GetItFreshApi.Entities
+﻿using GetItFreshApi.Entities.Enums;
+
+namespace GetItFreshApi.Entities
 {
     public class Pricing
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-
+        public PurchaseType PurchaseType { get; set; }
     }
 }
--- GetItFreshApi/Program.cs
+++ GetItFreshApi/Program.cs
@@ -1,25 +1,50 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo.File(path: "C:\\winks\\GetItFreshApi\\GetItFreshApi\\Log\\Log.txt")
+//    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var log = new LoggerConfiguration()
+    .WriteTo.File("Logs/Log.txt",
+    rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true)
+    .CreateLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+
 }
+catch (Exception ex)
+{
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    Log.Fatal(ex, "Application failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
