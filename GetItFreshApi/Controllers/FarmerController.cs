bcdd86bd4376079d95bcfc5f7d20be2a5761f15d
Akans Udoka <Akansudoka7@gmail.com>
2023-10-16 21:30:11 +0100

    trying

--- GetItFreshApi/Controllers/FarmerController.cs
+++ GetItFreshApi/Controllers/FarmerController.cs
@@ -13,7 +13,7 @@ namespace GetItFreshApi.Controllers
         private readonly IUnitOfWork unitOfWork;
         private readonly ILogger<FarmerController> logger;
         private readonly IMapper mapper;
-        public FarmerController(IUnitOfWork unitOfWork,  ILogger<FarmerController> logger, IMapper mapper)
+        public FarmerController(IUnitOfWork unitOfWork, ILogger<FarmerController> logger, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
             this.logger = logger;
@@ -27,7 +27,24 @@ namespace GetItFreshApi.Controllers
             try
             {
                 var farmers = await unitOfWork.Farmer.GetAll();
-                var result = mapper.Map < IList<FarmerDTO>>(farmers);
+                var result = mapper.Map<IList<FarmerDTO>>(farmers);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Something went wromg in the {nameof(GetAllFarmers)} endpoint");
+                return StatusCode(500, "Internal server error, please try again later");
+            }
+        }
+
+
+        [HttpGet("{id/int}")]
+        public async Task<IActionResult> GetAllFarmersById( int id)
+        {
+            try
+            {
+                var farmers = await unitOfWork.Farmer.Get(c => c.Id == id);
+                var result = mapper.Map<IList<FarmerDTO>>(farmers);
                 return Ok(result);
             }
             catch (Exception ex)
